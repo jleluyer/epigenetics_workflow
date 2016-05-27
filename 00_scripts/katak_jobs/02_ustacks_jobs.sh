22c439f88ea808cf7873767acc6e222303178026
jleluyer <jeremy.le.luyer@gmail.com>
2016-05-27 12:17:55 -0400

    Update 02_ustacks_jobs.sh

--- 00_scripts/katak_jobs/02_ustacks_jobs.sh
+++ 00_scripts/katak_jobs/02_ustacks_jobs.sh
@@ -3,11 +3,11 @@
 #SBATCH -D ./ 
 #SBATCH --job-name="ustacks"
 #SBATCH -o log-ustacks.out
-#SBATCH -c 1
+#SBATCH -c 4
 #SBATCH -p ibismini
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=type_your_mail@ulaval.ca
-#SBATCH --time=0-20:00
+#SBATCH --time=1-00:00
 #SBATCH --mem=50000
 
 cd $SLURM_SUBMIT_DIR
