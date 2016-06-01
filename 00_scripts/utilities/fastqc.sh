c4f4a2e647befc49a86600208cc2ecd686bda9b1
jeremyleluyer <jeremy.le.luyer@gmail.com>
2016-06-01 10:37:33 -0400

    added utilities

---
+++ 00_scripts/utilities/fastqc.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -D ./
+#SBATCH --job-name="fastqc"
+#SBATCH -o log-fastqc.out
+#SBATCH -c 1
+#SBATCH -p ibismini
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=type_your_mail@ulaval.ca
+#SBATCH --time=1-00:00
+#SBATCH --mem=20000
+
+cd $SLURM_SUBMIT_DIR
+
+mkdir fastqc_dir
+for file in $(ls 02_data/.*f*q.gz|sed 's/.f(ast)?q.gz//g')
+do
+base=$(basename $file)
+        mkdir  ./fastqc.dir/"$base".dir
+        fastqc -o ./fastqc.dir/"$base".dir -f fastq "$base".fastq.gz
+done
