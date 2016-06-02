b007f2d8b29707c19fcbce8707116053694ebff9
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-02 08:06:07 -0400

    Update 04_prepare_reference.sh

--- 00_scripts/04_prepare_reference.sh
+++ 00_scripts/04_prepare_reference.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zcat 03_trimmed/batch_1.catalog.tags.tsv.gz|grep -v '#'|awk '{print ">"$3,"\n"$10}' > 04_reference/reference_catalog.fasta
+zcat 03_trimmed/batch_1.catalog.tags.tsv.gz|grep -v '#'|awk '{print ">"$3"\n"$10}' > 04_reference/reference_catalog.fasta
