6cfcc20507f3caeedaa3caba4dd5a837b0d55824
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-02 08:56:51 -0400

    Create 01_trim_galore_gbs.sh

---
+++ 00_scripts/01_trim_galore_gbs.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+TIMESTAMP=$(date +%Y-%m-%d_%Hh%Mm%Ss)
+
+# Copy script as it was run
+SCRIPT=$0
+NAME=$(basename $0)
+LOG_FOLDER="98_log_files"
+cp $SCRIPT $LOG_FOLDER/"$TIMESTAMP"_"$NAME"
+
+'''usage
+trim_galore [options] <filename(s)>
+'''
+
+#variables
+LENGTH=90
+QUAL=20
+ERROR_RATE="0.2"
+for file in $(ls 02_data/*gbs*.fq.gz)
+do
+base=$(basename $file)
+
+trim_galore --length $LENGTH --no_report_file -e $ERROR_RATE --illumina -q $QUAL $base 
+
+done 2>&1 | tee 98_log_files/"$TIMESTAMP"_trimmgalore_gbs.log
