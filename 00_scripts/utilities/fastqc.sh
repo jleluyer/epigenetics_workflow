1c5f271f2cc3c7610523b2e94b0bbe6343f69bbb
jleluyer <jeremy.le.luyer@gmail.com>
2016-06-14 11:15:20 -0400

    changed fastqc check

--- 00_scripts/utilities/fastqc.sh
+++ 00_scripts/utilities/fastqc.sh
@@ -13,9 +13,9 @@
 cd $SLURM_SUBMIT_DIR
 
 mkdir fastqc_dir
-for file in $(ls 02_data/*.*f*q.gz|sed 's/.f(ast)?q.gz//g')
+for file in $(ls 02_data/*.*f*q.gz|sed 's/.fastq.gz//g')
 do
 base=$(basename $file)
         mkdir  ./fastqc_dir/"$base"_dir
-        fastqc -o ./fastqc_dir/"$base"_dir -f fastq "$base".fastq.gz
+        fastqc -o ./fastqc_dir/"$base"_dir -f fastq 02_data/"$base".fastq.gz
 done
