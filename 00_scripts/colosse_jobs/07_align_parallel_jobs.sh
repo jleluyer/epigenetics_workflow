bc1b1db2d2cf40733fc9f8f8f975c682e39d7a3f
jleluyer <jeremy.le.luyer@gmail.com>
2016-09-13 10:02:55 -0400

    add colosse parallel

---
+++ 00_scripts/colosse_jobs/07_align_parallel_jobs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd $(pwd)
+
+for file in $(ls 03_trimmed|sed 's/.fq.gz//g')
+
+do
+base="$(basename $file)"
+
+    toEval="cat 00_scripts/07_bismark_align_parallel_colosse.sh | sed 's/__BASE__/$base/g'"
+    eval $toEval > ALIGN_"$base".sh
+done
+
+# Submit jobs
+for i in $(ls ALIGN*sh)
+do
+    sbatch $i
+done
