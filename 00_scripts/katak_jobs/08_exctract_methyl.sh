26887b09a3c1929a2fb8ae60189becdbe1241de9
jleluyer <jeremy.le.luyer@gmail.com>
2017-01-31 05:42:30 -0500

    changes methylkit script

--- 00_scripts/katak_jobs/08_exctract_methyl.sh
+++ 00_scripts/katak_jobs/08_exctract_methyl.sh
@@ -4,11 +4,11 @@
 #SBATCH --job-name="EPIextract"
 #SBATCH -o log-extract.out
 #SBATCH -c 4
-#SBATCH -p low-cancel
-####SBATCH -A ibismax
+#SBATCH -p ibismax
+#SBATCH -A ibismax
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=type_your_mail@ulaval.ca
-#SBATCH --time=2-00:00
+#SBATCH --time=5-00:00
 #SBATCH --mem=20000
 
 cd $SLURM_SUBMIT_DIR
